ac17bf747dbc106f1cdd3cc9b5697f79a662af38
salko-ua <chips69jamil69game@gmail.com>
2024-08-25 23:18:08 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import openpyxl
-
-# from openpyxl.cell.cell import Cell
 import dataclasses
 import typing as t
-from pprint import pprint
-from random import choice
 import pathlib
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-import time
 from glob import glob
+from time import sleep
+
+# CONST
+WB = openpyxl.load_workbook("./Rozklad_23_24.xlsx")
+SHEET_FOR_PARSE = WB["Розклад"]
 
 
 @dataclasses.dataclass
@@ -73,53 +72,76 @@ class Schedule:
         )
 
 
-def get_data_from_sheet(wb, column: int):
-    sheet_for_parse = wb["Розклад"]
-    row = 6
+def get_data_from_sheet(column: int) -> list[list[str]]:
     result = []
-    column = 2 * column + 1
+    column = column * 2 + 1
+    start_row = 6
+    end_row = 67
 
-    for row in range(row, 67):
-        if ((row - 7) // 2) % 2:
-            color = "white"
-        else:
-            color = "gray"
+    for row in range(start_row, end_row):
+        value1 = SHEET_FOR_PARSE.cell(row=row, column=column).value
+        value2 = SHEET_FOR_PARSE.cell(row=row, column=column + 1).value
+        result.append([value1, value2])
 
-        value1 = sheet_for_parse.cell(row=row, column=column).value
-        value2 = sheet_for_parse.cell(row=row, column=column + 1).value
-
-        finish1 = f"""<td class="middle {color}">
-                    {value1}
-                </td>"""
-        finish2 = f"""<td class="end {color}">
-                    {value2}
-                </td>"""
-        if row == 6:
-            result.append([value1, value2])
-        elif value1 is not None and value2 is not None:
-            # print(row, value1, value2)
-            result.append([finish1, finish2])
-        elif value1 is not None and value2 is None:
-            # print(row, value1, value2)
-            result.append([finish1, f"<td class='end {color}'></td>"])
-        elif value1 is None and value2 is None:
-            result.append(
-                [
-                    f"<td class='middle clear {color}'></td>",
-                    f"<td class='end clear {color}'></td>",
-                ]
-            )
-        elif value1 is None and value2 is not None:
-            result.append([f"<td class='middle clear {color}'></td>", finish2])
-
-    result = cut_big_words(result)
-
-    return Schedule.from_dict(result)
+    return result
 
 
-def cut_big_words(list_: list[list]):
+def transform_list_to_html_list(list_: list[list[str]]) -> list[list[str]]:
+    result = []
+    for count, row in enumerate(list_):
+        # 2 gray ... 2 white etc.
+        color = "gray" if not ((count - 1) // 2) % 2 else "white"
+        middle_clear = f"<td class='middle clear {color}'></td>"
+        end_clear = f"<td class='end clear {color}'></td>"
+        middle = f"<td class='middle {color}'>{row[0]}</td>"
+        end = f"<td class='end {color}'>{row[1]}</td>"
+        if count == 0:
+            result.append([row[0], row[1]])
+        elif row[0] is not None and row[1] is not None:
+            result.append([middle, end])
+        elif row[0] is not None and row[1] is None:
+            result.append([middle, end_clear])
+        elif row[0] is None and row[1] is None:
+            result.append([middle_clear, end_clear])
+        elif row[0] is None and row[1] is not None:
+            result.append([middle_clear, end])
+    return result
 
-    return list_
+
+def cut_big_words(list_: list[list[str]]) -> list[list[str]]:
+    change_on_it = {
+        "Мистецтво (осн реставр)": "Мистецтво (осн реста)",
+        "Англійська мова (проф)": "Англ мова (проф)",
+        "Дизайн (Поліграфія) ІІ": "Дизайн (Поліграф.) ІІ",
+        "Інформ техн і комп граф": "ІТ і Комп граф",
+        "Встеп до спец (Технології)": "Вступ до спец (Техн)",
+        "Метод фіз / Метод технол": "Метод фіз / технол",
+        "Метод технол / Метод фіз вих": "Метод техн / фіз виховання",
+        "Туристична і краєзнавча": "Туристична і краєзнав",
+        "Польська / Інформаційні технолог": "Польска / Інформ техн",
+        "Інформаційні / Польська": "Інформ / Польська",
+        "Захист України (дів,юн)": "Зах. України (дів,юн)",
+    }
+    result = []
+    for row in list_:
+        cut_words = []
+        for word in row:
+            if len(f"{word}") > 21:
+                try:
+                    cut_words.append(change_on_it[f"{word}"])
+                except:
+                    cut_words.append(word)
+            else:
+                cut_words.append(word)
+        result.append(cut_words)
+    return result
+
+
+def get_finished_schedule_object(column: int) -> Schedule:
+    raw_data = get_data_from_sheet(column)
+    fried_data = cut_big_words(raw_data)
+    html_format = transform_list_to_html_list(fried_data)
+    return Schedule.from_dict(html_format)
 
 
 def get_first_block(element: str, lesson: int, schedules: Schedule):
@@ -222,24 +244,25 @@ def import_data_to_html(schedules: Schedule):
 
 
 def parse_all_schedules(count: int):
-    wb = openpyxl.load_workbook("./Rozklad_23_24.xlsx")
     # к-ть розкладів
     for i in range(1, count + 1):
-        schedules = get_data_from_sheet(wb, i)
+        schedules = get_finished_schedule_object(i)
         import_data_to_html(schedules)
 
 
 def parse_all_schedules_to_photo():
     driver = webdriver.Firefox()
-    for i in range(1):
+    for i in range(37):
         group_name = glob("./variant/*", recursive=True)[i]
         html_file = "file://" + f"/home/salo/huta/{group_name}"
         driver.get(html_file)
-        driver.set_window_size(height=1000, width=800)
+        driver.set_window_size(height=1200, width=1000)
         pathlib.Path(f"./screenshots").mkdir(parents=True, exist_ok=True)
         driver.save_screenshot(f"./screenshots/{group_name[9:]}.png")
+        sleep(10)
     # driver.quit()
 
 
 if "__main__" == __name__:
     parse_all_schedules(38)
+    parse_all_schedules_to_photo()
